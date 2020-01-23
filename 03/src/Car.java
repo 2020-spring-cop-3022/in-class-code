28702e57a42fcdbb9a1af34affeb7d1b60f59dbf
Professor Bitner <sbitner@uwf.edu>
2020-01-23 14:51:50 -0600

    Adding car count functionality

--- 03/src/Car.java
+++ 03/src/Car.java
@@ -2,18 +2,25 @@
 public class Car
 {
 	private String color;
+	private static int carCount = 0;
 	
 	public Car ()
 	{
-		
+		this("grey");
 	}
 	public Car (String color)
 	{
 		this.color = color;
+		++Car.carCount;
 	}
 	
 	public String getColor ()
 	{
 		return this.color;
+	}
+	
+	public static int getCarCount ()
+	{
+		return Car.carCount;
 	}
 }
--- 03/src/CarTest.java
+++ 03/src/CarTest.java
@@ -8,9 +8,13 @@ class CarTest {
 	void test() {
 		//String foo = new String("bar");
 		Car jalopy = new Car();
+		assertEquals("grey", jalopy.getColor());
+
 		Car corvette = new Car("red");
 		
 		assertEquals("red", corvette.getColor());
+		
+		assertEquals(2, Car.getCarCount());
 	}
 
 }
