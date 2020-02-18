22e9df4892e2deb297d709b675f03ebad03763cd
Professor Bitner <sbitner@uwf.edu>
2020-02-18 15:28:08 -0600

    Adding specific exception handling

--- 05-exceptions/src/ExceptionTester.java
+++ 05-exceptions/src/ExceptionTester.java
@@ -17,10 +17,14 @@ class ExceptionTester {
 			int nameLength = names[0].length();
 			fail("exception not throw");
 		}
-		catch (Exception e)
+		catch (NullPointerException e)
 		{
 			assertEquals(1, 1);
 			System.out.println("Exception caught: "+e.getMessage());
+		}
+		catch (Exception e)
+		{
+			fail("base exception thrown");
 		}
 
 		System.out.println("We made it");
