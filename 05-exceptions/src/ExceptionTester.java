a9ac0f7d4246e5c981c11f8c9cff9da92e73b843
Professor Bitner <sbitner@uwf.edu>
2020-02-18 15:45:48 -0600

    End of lecture - Added custom exception class - Added finally with While loop control example

--- 05-exceptions/src/ExceptionTester.java
+++ 05-exceptions/src/ExceptionTester.java
@@ -11,23 +11,34 @@ class ExceptionTester {
 		assertThrows(NullPointerException.class,
 				() -> { int nameLength = names[0].length(); });
 		
-		// using common syntax
-		try
+		while (true)
 		{
-			int nameLength = names[0].length();
-			fail("exception not throw");
+			// using common syntax
+			try
+			{
+				int nameLength = names[0].length();
+				fail("exception not throw");
+				throw new UnauthorizedUserException("foo");
+			}
+			catch (NullPointerException e)
+			{
+				assertEquals(1, 1);
+				System.out.println("Exception caught: "+e.getMessage());
+				break;
+			}
+			catch (UnauthorizedUserException e)
+			{
+				// do something
+			}
+			catch (Exception e)
+			{
+				fail("base exception thrown");
+			}
+			finally
+			{
+				System.out.println("We made it");
+			}
 		}
-		catch (NullPointerException e)
-		{
-			assertEquals(1, 1);
-			System.out.println("Exception caught: "+e.getMessage());
-		}
-		catch (Exception e)
-		{
-			fail("base exception thrown");
-		}
-
-		System.out.println("We made it");
 	}
 
 }
---
+++ 05-exceptions/src/UnauthorizedUserException.java
@@ -0,0 +1,8 @@
+
+public class UnauthorizedUserException extends Exception
+{
+	public UnauthorizedUserException (String message)
+	{
+		super(message);
+	}
+}
