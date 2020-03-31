dcc72d4b3ed4d927afa7b3f63addd8956503ad2e
Professor Steven Bitner <sbitner@uwf.edu>
2020-03-31 15:46:58 -0500

    Adding spinner and using pack for sizing

--- 09_GUI/src/SampleFrame.java
+++ 09_GUI/src/SampleFrame.java
@@ -3,6 +3,8 @@ import java.awt.BorderLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JSpinner;
+import javax.swing.SpinnerNumberModel;
 
 public class SampleFrame extends JFrame
 {
@@ -10,7 +12,7 @@ public class SampleFrame extends JFrame
 	{
 		super();
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		this.setSize(100, 100);
+		//this.setSize(100, 100);
 	}
 	
 	public static void main (String[] args)
@@ -22,11 +24,18 @@ public class SampleFrame extends JFrame
 		frame.add(button);
 		
 		JLabel someText = new JLabel("UWF");
+		someText.setHorizontalAlignment(JLabel.CENTER);
 		frame.add(someText, BorderLayout.SOUTH);
-
-		// layout
 		
+		// anonymous
+		frame.add(new JButton("Foo"), BorderLayout.NORTH);
+		
+		SpinnerNumberModel nums = new SpinnerNumberModel(150, 0, 200, 1);
+		JSpinner wheel = new JSpinner(nums);
+		frame.add(wheel, BorderLayout.EAST);
+
 		// show me
+		frame.pack();
 		frame.setVisible(true);
 	}
 }
